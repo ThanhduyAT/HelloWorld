b3a9077616b3370689e9b6c26e243bfce568712f
thanhduyAT <thanhduy2198@gmail.com>
2021-04-05 12:32:38 +0700

    "hello"

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by hd on 4/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregasd
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by hd on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
